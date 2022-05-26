ceb7343cfe2df3dd11f2fc552e02196670c5649c
bruhboy666 <84838343+bruhboy666@users.noreply.github.com>
2022-05-26 23:07:21 +0300

    Create Pressure Wash Simulator infinite Cash.lua

---
+++ Pressure Wash Simulator infinite Cash.lua
@@ -0,0 +1,4 @@
+local Dirt = 999999999999999999
+local v= -147.53421020508, 33.283451080322, 134.66027832031
+
+game:GetService("ReplicatedStorage").Remotes.ClientFrameData:FireServer(Dirt , true, true, v)
