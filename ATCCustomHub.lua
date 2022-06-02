a1b5d68dca32fc0c9f6aa94db15fe500da537682
bruhboy666 <84838343+bruhboy666@users.noreply.github.com>
2022-06-02 20:29:27 +0300

    Add files via upload

---
+++ ATCCustomHub.lua
@@ -0,0 +1,325 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("", "Synapse")
+
+local UniversalTab = Window:NewTab("Main")
+local UniversalSection1 = UniversalTab:NewSection("Character")
+
+UniversalSection1:NewSlider("Speed", "Karakterinizin Hızını Ayarlar.", 500, 16, function(s)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+UniversalSection1:NewSlider("Jump Power", "Karakterinizin Zıplama Gücünü Ayarlar", 500, 50, function(jp)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = jp
+end)
+
+UniversalSection1:NewButton("Fling GUI", "Fling sciript Askeriye saldırılarında kullanılır", function()
+    local troller = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
+local nameofgui = Instance.new("TextLabel")
+local border = Instance.new("Frame")
+local invis = Instance.new("TextButton")
+local pp = Instance.new("TextButton")
+local memedog = Instance.new("TextLabel")
+local die = Instance.new("TextLabel")
+local worm = Instance.new("TextButton")
+local cycl = Instance.new("TextButton")
+local axy = Instance.new("TextLabel")
+local diemie = Instance.new("TextLabel")
+
+troller.Name = "troller"
+troller.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+troller.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+troller.ResetOnSpawn = false
+
+Main.Name = "Main"
+Main.Parent = troller
+Main.BackgroundColor3 = Color3.new(0.129412, 0.129412, 0.129412)
+Main.BorderSizePixel = 0
+Main.ClipsDescendants = true
+Main.Position = UDim2.new(0.04588015, 0, 0.087649405, 0)
+Main.Size = UDim2.new(0, 248, 0, 311)
+
+nameofgui.Name = "nameofgui"
+nameofgui.Parent = Main
+nameofgui.BackgroundColor3 = Color3.new(1, 1, 1)
+nameofgui.BackgroundTransparency = 1
+nameofgui.ClipsDescendants = true
+nameofgui.Size = UDim2.new(0, 248, 0, 19)
+nameofgui.Font = Enum.Font.GothamBold
+nameofgui.Text = "ATC Hub"
+nameofgui.TextColor3 = Color3.new(1, 1, 1)
+nameofgui.TextSize = 16
+nameofgui.TextXAlignment = Enum.TextXAlignment.Left
+
+border.Name = "border"
+border.Parent = Main
+border.BackgroundColor3 = Color3.new(1, 1, 1)
+border.ClipsDescendants = true
+border.Position = UDim2.new(-0.00131386518, 0, 0.0696422011, 0)
+border.Size = UDim2.new(0, 248, 0, 1)
+
+invis.Name = "invis"
+invis.Parent = Main
+invis.BackgroundColor3 = Color3.new(1, 0.541176, 0.164706)
+invis.ClipsDescendants = true
+invis.Position = UDim2.new(0, 0, 0.115512639, 0)
+invis.Size = UDim2.new(0, 248, 0, 27)
+invis.Font = Enum.Font.SourceSansItalic
+invis.Text = "FE Invisible Fling (R6 & R15)"
+invis.TextColor3 = Color3.new(1, 1, 1)
+invis.TextSize = 16
+invis.MouseButton1Click:connect(function()
+	
+
+
+spawn(function()
+local message = Instance.new("Message",workspace)
+message.Text = "Loaded press z to execute inviseble , press x to respawn)"
+wait(5)
+message:Destroy()
+end)
+
+
+
+
+
+
+
+
+local mouse = game.Players.LocalPlayer:GetMouse()
+
+local groot = nil
+
+mouse.KeyDown:connect(function(k)
+	
+	if k == "z" then
+		
+		
+		
+spawn(function()
+local message = Instance.new("Message",workspace)
+message.Text = "Fe Invisible Fling By Bruhboy 666#8107 Loaded (wait 11 seconds to load)"
+wait(11)
+message:Destroy()
+end)
+
+
+local ch = game.Players.LocalPlayer.Character
+local prt=Instance.new("Model", workspace)
+local z1 =  Instance.new("Part", prt)
+z1.Name="Torso"
+z1.CanCollide = false
+z1.Anchored = true
+local z2  =Instance.new("Part", prt)
+z2.Name="Head"
+z2.Anchored = true
+z2.CanCollide = false
+local z3 =Instance.new("Humanoid", prt)
+z3.Name="Humanoid"
+z1.Position = Vector3.new(0,9999,0)
+z2.Position = Vector3.new(0,9991,0)
+ game.Players.LocalPlayer.Character=prt
+wait(5)
+game.Players.LocalPlayer.Character=ch
+wait(6)
+
+
+local plr = game.Players.LocalPlayer
+mouse = plr:GetMouse()
+
+local Hum = Instance.new("Humanoid")
+Hum.Parent = game.Players.LocalPlayer.Character
+
+
+local root =  game.Players.LocalPlayer.Character.HumanoidRootPart
+
+
+for i,v in pairs(plr.Character:GetChildren()) do
+	
+	if v ~= root and  v.Name ~= "Humanoid" then
+		
+		v:Destroy()
+		
+	end
+	
+	
+end
+           
+workspace.CurrentCamera.CameraSubject = root
+
+local se = Instance.new("SelectionBox",root)
+se.Adornee = root
+
+
+game:GetService('RunService').Stepped:connect(function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CanCollide = false
+end)
+game:GetService('RunService').RenderStepped:connect(function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CanCollide = false
+end)
+
+
+power = 999999 -- change this to make it more or less powerful
+
+power = power*10
+
+---
+wait(.1)
+local bambam = Instance.new("BodyThrust")
+bambam.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
+bambam.Force = Vector3.new(power,0,power)
+bambam.Location = game.Players.LocalPlayer.Character.HumanoidRootPart.Position 
+
+
+
+
+
+local plr = game.Players.LocalPlayer
+local torso = root
+local flying = true
+local deb = true
+local ctrl = {f = 0, b = 0, l = 0, r = 0}
+local lastctrl = {f = 0, b = 0, l = 0, r = 0}
+local maxspeed = 120
+local speed = 15
+
+
+---local bambam = Instance.new("BodyThrust")
+---bambam.Parent = torso
+--bambam.Force = Vector3.new(9999999,0,9999999)
+--bambam.Location = torso.Position
+
+
+---
+groot = root
+ 
+function Fly()
+local bg = Instance.new("BodyGyro", torso)
+bg.P = 9e4
+bg.maxTorque = Vector3.new(0, 0, 0)
+bg.cframe = torso.CFrame
+local bv = Instance.new("BodyVelocity", torso)
+bv.velocity = Vector3.new(0,0,0)
+bv.maxForce = Vector3.new(9e9, 9e9, 9e9)
+repeat wait()
+
+if ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0 then
+speed = speed+.2
+if speed > maxspeed then
+speed = maxspeed
+end
+elseif not (ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0) and speed ~= 0 then
+speed = speed-1
+if speed < 0 then
+speed = 0
+end
+end
+if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then
+bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (ctrl.f+ctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(ctrl.l+ctrl.r,(ctrl.f+ctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed
+lastctrl = {f = ctrl.f, b = ctrl.b, l = ctrl.l, r = ctrl.r}
+elseif (ctrl.l + ctrl.r) == 0 and (ctrl.f + ctrl.b) == 0 and speed ~= 0 then
+bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (lastctrl.f+lastctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(lastctrl.l+lastctrl.r,(lastctrl.f+lastctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed
+else
+bv.velocity = Vector3.new(0,0.1,0)
+end
+
+until not flying
+ctrl = {f = 0, b = 0, l = 0, r = 0}
+lastctrl = {f = 0, b = 0, l = 0, r = 0}
+speed = 0
+bg:Destroy()
+bv:Destroy()
+
+end
+mouse.KeyDown:connect(function(key)
+if key:lower() == "e" then
+if flying then flying = false
+else
+flying = true
+Fly()
+end
+elseif key:lower() == "w" then
+ctrl.f = 1
+elseif key:lower() == "s" then
+ctrl.b = -1
+elseif key:lower() == "a" then
+ctrl.l = -1
+elseif key:lower() == "d" then
+ctrl.r = 1
+end
+end)
+mouse.KeyUp:connect(function(key)
+if key:lower() == "w" then
+ctrl.f = 0
+elseif key:lower() == "s" then
+ctrl.b = 0
+elseif key:lower() == "a" then
+ctrl.l = 0
+elseif key:lower() == "d" then
+ctrl.r = 0
+elseif key:lower() == "r" then
+
+end
+end)
+Fly()
+
+		
+		
+	elseif k == "x" then
+		
+		
+		spawn(function()
+local message = Instance.new("Message",workspace)
+message.Text = "Respawning dont spam"
+wait(1)
+message:Destroy()
+end)
+		
+		local saved = groot.Position
+		
+local ch = game.Players.LocalPlayer.Character
+local prt=Instance.new("Model", workspace)
+local z1 =  Instance.new("Part", prt)
+z1.Name="Torso"
+z1.CanCollide = false
+z1.Anchored = true
+local z2  =Instance.new("Part", prt)
+z2.Name="Head"
+z2.Anchored = true
+z2.CanCollide = false
+local z3 =Instance.new("Humanoid", prt)
+z3.Name="Humanoid"
+z1.Position = Vector3.new(0,9999,0)
+z2.Position = Vector3.new(0,9991,0)
+ game.Players.LocalPlayer.Character=prt
+wait(5)
+ game.Players.LocalPlayer.Character=ch
+local poop = nil
+		repeat wait() poop = game.Players.LocalPlayer.Character:FindFirstChild("Head") until poop ~= nil
+		wait(1)
+		game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(saved)
+		
+	end
+	
+	
+end)
+end)
+end)
+
+if game.PlaceId == 1224212277 then
+    local FarmTab = Window:NewTab("Auto Farm")
+    local FarmSection = FarmTab:NewSection("Auto Farm")
+
+    FarmSection:NewButton("Auto Farm", "Turns on auto farm", function()
+        game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer("SetTeam", "Police")
+wait(.75)
+game:GetService("RunService").RenderStepped:Connect(function()
+for i,v in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
+   if v.Name == "Handcuffs" then v.Parent = game:GetService("Players").LocalPlayer.Character
+   end
+end
+game:GetService("ReplicatedStorage").Event:FireServer("Eject", game:GetService("Players").LocalPlayer)
+end)
+    end)
+
+end
